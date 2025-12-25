d9515712e96519171431e72482495b7998c01b49
Pratap Suryawanshi <pratapsuryawanshi98@gmail.com>
2025-12-25 17:59:26 +0530

    app.py file updated with small change

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, DevOps Learner! My CI/Cd pipeline is working!"
+    return "Hello, DevOps Learner! My CI/Cd pipeline is working! Bye!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
